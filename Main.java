b07c53ddcd667329eefeb2ffaabd7c2ac8762799
Jon <60653854+jonf7@users.noreply.github.com>
2021-10-06 07:57:39 +0300

    Update and rename README.md to Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new MyFrame();
+
+    }
+}
